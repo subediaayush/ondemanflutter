ef35961bc1b13abb2e25c776b2227f2e0d1804ce
Aayush Subedi <subediaayush@gmail.com>
2020-05-04 12:33:14 +0545

    Add Flutter Activity

--- dynamicfeature/src/main/java/com/ondemandflutter/dynamicfeature/FlutterEntry.kt
+++ dynamicfeature/src/main/java/com/ondemandflutter/dynamicfeature/FlutterEntry.kt
@@ -1,18 +1,24 @@
 package com.ondemandflutter.dynamicfeature
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import io.flutter.embedding.android.FlutterActivity
+import com.google.android.play.core.splitcompat.SplitCompat
 
 class FlutterEntry : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        FlutterActivity.withNewEngine().build(this).apply {
+        FlutterEntryActivity.EngineBuilder().build(this).apply {
             startActivity(this)
         }
 
         finish()
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+        SplitCompat.installActivity(this)
     }
 }
